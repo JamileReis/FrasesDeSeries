0ecc6f6f7fb05798a12b1036031db49d78bb583f
Jamile Matos dos Reis <140525086+JamileReis@users.noreply.github.com>
2024-10-14 10:53:59 -0300

    new commit

--- src/main/java/com/estudo/screenmatch_frases/Frase.java
+++ src/main/java/com/estudo/screenmatch_frases/Frase.java
@@ -1,16 +1,25 @@
-package com.estudo.screenmatch_frases.model;
+package com.estudo.screenmatch_frases;
 
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "frases")
+@Table(name="frases")
 public class Frase {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String titulo;
+    private String frase;
     private String personagem;
     private String poster;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getTitulo() {
         return titulo;
@@ -18,6 +27,14 @@ public class Frase {
 
     public void setTitulo(String titulo) {
         this.titulo = titulo;
+    }
+
+    public String getFrase() {
+        return frase;
+    }
+
+    public void setFrase(String frase) {
+        this.frase = frase;
     }
 
     public String getPersonagem() {
@@ -34,13 +51,5 @@ public class Frase {
 
     public void setPoster(String poster) {
         this.poster = poster;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
     }
 }
